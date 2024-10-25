839d3d34607bd5ca826c119cc63ab9e0210affbd
Foxycrack12d <frabel791@gmail.com>
2024-10-25 13:04:02 -0700

    Add files via upload

---
+++ Tienda/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" />
+    <title>Dia De Muertos</title>
+</head>
+
+<body>
+    <div id="container">
+        
+        <header id="header">
+            <div id="logo">
+               <img src="img/descarga.jpeg" alt="Logo">
+               <a href="index.php">Dia De Muertos</a>
+            </div>
+        </header>
+
+        <div id="content">
+
+            <aside id="lateral">
+                <div id="login" class="block_aside">
+                    <h3>Entrar a la web</h3>
+                    <form action="#" method="post">
+                        <label for="email">EMAIL</label>
+                        <input type="email" name="email" />
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password" />
+                        <input type="submit" name="Iniciar Sesion" />
+                    </form>
+                    <ul>
+                        <li><a href="#">Informacion</a></li>
+                        <li><a href="#">Imagenes</a></li>
+                        <li><a href="#">Que es?</a></li>
+                    </ul>
+                </div>
+                
+            </aside>
+
+            <div id="central">
+                <h1>Informacion</h1>
+
+                <div class="product">
+                    <img src="img/Altar.jpeg" alt="Guitarra">
+                    <h2>Los altares son una de las tradiciones más arraigadas dentro de la celebración del Día de Muertos del 31 de octubre al 2 de noviembre en México, considerada Patrimonio Cultural Inmaterial de la Humanidad por la UNESCO, la cual consiste en colocar ofrendas para honrar a los seres queridos difuntos.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>                
+                <div class="product">
+                    <img src="img/Cempasuchil.jpeg" alt="Guitarra">
+                    <h2>La tradición marca hacer senderos con las flores de cempasúchil, desde el camino principal hasta el altar de la casa con la finalidad de guiar a las almas hacia los altares. La flor era considerada por los mexicas como un símbolo de vida y muerte.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>                
+                <div class="product">
+                    <img src="img/Catrina.jpeg" alt="Guitarra">
+                    <h2>La Catrina es un personaje creado por el pintor, ilustrador y caricaturista mexicano de Aguascalientes, José Guadalupe Posada, es la figura que más se asocia al Día de Muertos y a la muerte en general en nuestro país, siendo uno de los iconos con los que México es conocido en el mundo entero.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>   
+                <div class="product">
+                    <img src="img/Agua.jpeg" alt="Guitarra">
+                    <h2>La fuente de la vida, se ofrece a las ánimas para que mitiguen su sed después de su largo recorrido y para que fortalezcan su regreso. En algunas culturas simboliza la pureza del alma.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>                  
+                <div class="product">
+                    <img src="img/Copal.jpeg" alt="Guitarra">
+                    <h2>El copal era ofrecido por los indígenas a sus dioses ya que el incienso aún no se conocía, este llegó con los españoles. Es el elemento que sublima la oración o alabanza. Fragancia de reverencia. Se utiliza para limpiar al lugar de los malos espíritus y así el alma pueda entrar a su casa sin ningún peligro.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>     
+                <div class="product">
+                    <img src="img/descarga.jpeg" alt="Guitarra">
+                    <h2>Las calaveras de azúcar medianas son alusión a la muerte siempre presente. Las calaveras chicas son dedicadas a la Santísima Trinidad y la grande al Padre Eterno.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>     
+                <div class="product">
+                    <img src="img/izcuintle.jpeg" alt="Guitarra">
+                    <h2>Lo que no debe faltar en los altares para niños es el perrito izcuintle en juguete, para que las ánimas de los pequeños se sientan contentas al llegar al banquete. El perrito izcuintle, es el que ayuda a las almas a cruzar el caudaloso río Chiconauhuapan, que es el último paso para llegar al Mictlán.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>     
+                <div class="product">
+                    <img src="img/Mole.jpeg" alt="Guitarra">
+                    <h2>El mole con pollo, gallina o guajolote, es el platillo favorito que ponen en el altar muchos indígenas de todo el país, aunque también le agregan barbacoa con todo y consomé. Estos platillos son esa estela de aromas, el banquete de la cocina en honor de los seres recordados. La buena comida tiene por objeto deleitar al ánima que nos visita.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>     
+                <div class="product">
+                    <img src="img/Pan de muertos.jpeg" alt="Guitarra">
+                    <h2> El ofrecimiento fraternal es el pan. La iglesia lo presenta como el "Cuerpo de Cristo". Elaborado de diferentes formas, el pan es uno de los elementos más preciados en el altar.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>     
+                <div class="product">
+                    <img src="img/Petate.jpeg" alt="Guitarra">
+                    <h2>Entre los múltiples usos del petate se encuentra el de cama, mesa o mortaja. En este particular día funciona para que las ánimas descansen así como de mantel para colocar los alimentos de la ofrenda.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>     
+                <div class="product">
+                    <img src="img/Sal.jpeg" alt="Guitarra">
+                    <h2>El elemento de purificación, sirve para que el cuerpo no se corrompa, en su viaje de ida y vuelta para el siguiente año.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>     
+                <div class="product">
+                    <img src="img/Velas.jpeg" alt="Guitarra">
+                    <h2>Los antiguos mexicanos utilizaban rajas de ocote. En la actualidad se usa el cirio en sus diferentes formas: velas, veladoras o ceras. La flama que producen significa "la luz", la fe, la esperanza. Es guía, con su flama titilante para que las ánimas puedan llegar a sus antiguos lugares y alumbrar el regreso a su morada. En varias comunidades indígenas cada vela representa un difunto, es decir, el número de veladoras que tendrá el altar dependerá de las almas que quiera recibir la familia. Si los cirios o los candeleros son morados, es señal de duelo; y si se ponen cuatro de éstos en cruz, representan los cuatro puntos cardinales, de manera que el ánima pueda orientarse hasta encontrar su camino y su casa.</h2>
+                    <p></p>
+                    <a href="#" class="button">Más Informacion</a>
+                </div>      
+            </div>
+        </div>
+        <footer id="footer">
+            <p>Desarrollado por: Francsico Ivan Beltran Romero&copy; <?=date('Y') ?> </p>
+        </footer>
+    </div>
+    
+</body>
+</html>
